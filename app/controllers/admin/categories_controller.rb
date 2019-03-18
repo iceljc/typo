7e35ab9750b54013f6e3be350feed681fd45ed2a
EC2 Default User <ec2-user@ip-172-31-83-52.ec2.internal>
2019-03-18 20:49:53 +0000

    fix bug

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -25,13 +25,13 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
-    @category = case params[:id]
-    when nil
-      Category.new
-    else
-      Category.find(params[:id])
-    end
     # @category = Category.find(params[:id])
+    if params[:id].nil?
+      @category = Category.new
+    else
+      @category = Category.find(params[:id])
+    end
+    
     @category.attributes = params[:category]
     if request.post?
       respond_to do |format|
--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -15,6 +15,30 @@ describe Admin::CategoriesController do
     get :index
     assert_response :redirect, :action => 'index'
   end
+  
+  ### add
+  it "test_create" do
+    cat = FactoryGirl.create(:category)
+    Category.should_receive(:find).with(:all).and_return([])
+    Category.should_receive(:new).and_return(cat)
+    cat.should_receive(:save!).and_return(true)
+    post :edit, 'category' => { :name => "test category" }
+    expect(response).to redirect_to(:action => 'new')
+  end
+  
+  describe "test_new" do
+    before(:each) do
+      get :new
+    end
+
+    it 'should render template view' do
+      assert_template 'new'
+      assert_tag :tag => "table", :attributes => { :id => "category_container" }
+    end
+  end
+  
+  ###
+  
 
   describe "test_edit" do
     before(:each) do
